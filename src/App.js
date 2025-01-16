b2a8cf638de41d54d8057cd0a927f4ec5c5fb97d
MoApplesNOranges <billmo0120@gmail.com>
2025-01-16 14:16:08 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
 const App = () => {
-  return "Hello";
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "William Mo")
+  );
 };
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
