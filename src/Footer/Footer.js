16a653ac66ad925db3a4de22b8d8ad20b6ecf0bf
MoApplesNOranges <billmo0120@gmail.com>
2026-03-02 07:30:40 -0500

    changed footer/navbar

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,8 +1,11 @@
 import React from "react";
-import "./Footer.css";
 
 const Footer = () => {
-  return <h3 className="footer">Copyright 2025</h3>;
+  return (
+    <h3 className=" bottom-0 left-0 right-0 text-center py-4 bg-indigo-600 text-white">
+      Copyright 2025
+    </h3>
+  );
 };
 
 export default Footer;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,22 +1,42 @@
 import React from "react";
-import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <NavLink to="/">Will Mo</NavLink>
-      </div>
-      <div className="navbar-right">
-        <ul className="nav-links">
-          <li>
-            <NavLink to="/projects">Projects</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
+    <nav className="fixed top-0 w-full z-50 bg-indigo-600 shadow-lg backdrop-blur-md border-b border-gray-800">
+      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+        {/* Logo */}
+        <NavLink
+          to="/"
+          className="text-xl font-bold text-white hover:text-indigo-500 transition"
+        >
+          Will Mo
+        </NavLink>
+
+        {/* Links */}
+        <div className="flex gap-8 text-white font-medium">
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              `hover:text-indigo-500 transition ${
+                isActive ? "text-indigo-500" : ""
+              }`
+            }
+          >
+            Projects
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              `hover:text-indigo-500 transition ${
+                isActive ? "text-indigo-500" : ""
+              }`
+            }
+          >
+            Contact
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
